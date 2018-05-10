c08951341cc9012f516ed4a1b56bd84ebcd02f68
lu <1023973648@qq.com>
2018-05-10 09:29:48 +0800

    创建了张三activity

---
+++ app/src/main/java/com/example/lu/thebarbershop/Activity/zhangsanActivity.java
@@ -0,0 +1,15 @@
+package com.example.lu.thebarbershop.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lu.thebarbershop.R;
+
+public class zhangsanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhangsan);
+    }
+}
